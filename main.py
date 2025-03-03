02b37289a3102382c3377c392551309f04b457af
sgsg <1340172779@qq.com>
2025-03-03 18:04:02 +0800

    fix

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from Crawler import runCrawler
 from utility import genDetailDoc
-# runCrawler("CVE 漏洞库", 2, 0, 2)
-# runCrawler("非CVE漏洞库", 2, 0, 2)
-# runCrawler("高危漏洞", 2, 0, 2)
+runCrawler("CVE 漏洞库", 2, 0, 2)
+runCrawler("非CVE漏洞库", 2, 0, 2)
+runCrawler("高危漏洞", 2, 0, 2)
 
 enableAI = 1  # 是否开启AI生成报告功能
 
